cd7fc9f1be9580f64471e571443b3dc01b543381
dipvijay212 <148552241+dipvijay212@users.noreply.github.com>
2025-04-09 06:50:57 +0530

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,26 +1,26 @@
 const Book = require('../models/book');
 
-// GET all books
+// GET 
 exports.getAllBooks = async (req, res) => {
   const books = await Book.find();
   res.json(books);
 };
 
-// POST a new book
+// POST
 exports.createBook = async (req, res) => {
   const book = new Book(req.body);
   await book.save();
   res.status(201).json(book);
 };
 
-// PUT update a book
+// update
 exports.updateBook = async (req, res) => {
   const { id } = req.params;
   const book = await Book.findByIdAndUpdate(id, req.body, { new: true });
   res.json(book);
 };
 
-// DELETE a book
+// DELETE
 exports.deleteBook = async (req, res) => {
   const { id } = req.params;
   await Book.findByIdAndDelete(id);
